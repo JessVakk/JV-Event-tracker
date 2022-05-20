22ca09345556d2f381dde4ed1d0859066266b852
Jessica Vakkuri <jessica.vakkuri@utb.ecutbildning.se>
2022-05-20 10:52:55 +0200

    går att navigera

--- jessicas-event/src/views/AddEventView.jsx
+++ jessicas-event/src/views/AddEventView.jsx
@@ -1,96 +1,118 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { addEvent } from '../store/actions/eventsAction'
 import { useNavigate } from 'react-router-dom'
 
 
-const AddEventView = () => {
+function AddEventView() {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const loading  = useSelector(state => state.events.loading)
+  
 
- const [eventAdded, setEventAdded] = useState(false)
- const [error, setError] = useState(false)
+  const initialValues = { title: '', place: '', date: '', time: '', description: '' };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    place: '',
-    date: '',
-    time: ''
-  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-  const onChange = e => {
-    setFormData(state => ({
-      ...state,
-      [e.target.name]: e.target.value
-      
-    }))
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    
-    if(formData.title.trim()==='') {
-      setError(true)
-      return
-    } else if(formData.description.trim()==='') {
-      setError(true)
-      return
-    }
-      
-
-    setError(false)
-    dispatch(addEvent(formData))
-    setEventAdded(true)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    dispatch(addEvent(formValues))
+    setIsSubmit(true);
+  };
 
   useEffect(() => {
-    if(!loading && eventAdded) {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    
+    if (!values.title) {
+      errors.title = "Title is required";
+    } else if (values.title.length < 4) {
+      errors.title = "Title must be more than 4 characters";
+    }
+    if (!values.place) {
+      errors.place = "Place is required";
+    }
+   
+    if (!values.date) {
+      errors.date = "Date is required";
+    }
+    if (!values.time) {
+      errors.time = "Time is required";
+    }
+    if (!values.description) {
+      errors.description = "Description is required";
+    } else if (values.description.length < 4) {
+      errors.description = "Password must be more than 4 characters";
+    } 
+    return errors;
+  };
+
+  useEffect(() => {
+    if(!loading && isSubmit) {
       navigate('/')
     }
-  }, [loading, eventAdded, navigate])
-
+  }, [loading, isSubmit, navigate])
+  
   return (
+    <div className="container">
     
-          <div className='mt-5 container card py-5 rounded-7 shadow p-3 mb-5 bg-body rounded'>
-          <h2  className='text-center mb-4'>Add Event</h2>
-          <form onSubmit={handleSubmit} className='pe-5 ps-5 d-flex justify-content-center'>
-            <div className="row">
-              <div className="col-lg-12 mb-4 pe-lg-2" >
-              <label htmlFor="title">Title: </label>
-              <input type="text" id='title' name='title' onChange={onChange} value={formData.title} className='form-control' />
-              {error && <p className='error text-danger'>You can't leave this empty</p>}
-              </div> 
-              <div className="col-lg-6 col-sm-12 mb-4 ps-lg-2" >
-                <label htmlFor="date">Date: </label>
-                <input type="date" id='date' name='date' onChange={onChange} value={formData.date} className='form-control' />       
-              </div>
-              <div className="col-lg-6 col-sm-12 mb-4 ps-lg-2">
-                <label htmlFor="time">Time: </label>       
-                <input type="time" id='time' name='time' onChange={onChange} value={formData.time} className='form-control' />
-              </div>
-              <div className="col-lg-12  mb-4 pe-lg-2">
-              <label htmlFor="eventDesc">Event description: </label>       
-                <textarea type="text" id='description' name='description' onChange={onChange} value={formData.description} className='form-control' />
-                {error && <p className='error text-danger'>You can't leave this empty</p>}
-              </div>
-              
-           
-              <div>
-                <button className='mb-4 btn btn-block text-light gradient-color'> { loading ?'Loading...': 'Add Event'}</button>
-              </div>
-              
-              </div>
-            </form>
+
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>title</label>
+            <input type="text" name="title" value={formValues.title} onChange={handleChange}/>
+          </div>
+          <p>{formErrors.title}</p>
+
+          <div className="field">
+            <label>place</label>
+            <input type="text" name="place" value={formValues.place} onChange={handleChange}/>
+          </div>
+          <p>{formErrors.place}</p>
           
-            
+
+          <div className="field">
+            <label>Date</label>
+            <input type="date" name="date" value={formValues.date} onChange={handleChange}/>
+          </div>
+          <p>{formErrors.date}</p>
+          
+          <div className="field">
+            <label>Time</label>
+            <input
+              type="time" name="time" value={formValues.time} onChange={handleChange} />
+          </div>
+          <p>{formErrors.time}</p>
+
+          <div className="field">
+            <label>description</label>
+            <input
+              type="text" name="description" value={formValues.description} onChange={handleChange} />
+          </div>
+          <p>{formErrors.description}</p>
+
+          <button className='mb-4 btn btn-block text-light gradient-color'> { loading ?'Loading...': 'Add Event'}</button>
         </div>
-         
-         
-  )
+      </form>
+    </div>
+  );
 }
 
 export default AddEventView
